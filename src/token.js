a316794a8dd6aa080ac3cbf9f01ac6b98da84cfc
Leon <leonv11@gmx.net>
2024-01-06 19:07:22 +0100

    Synced with local source

--- src/token.js
+++ src/token.js
@@ -1,32 +1,53 @@
 const Tokens = {};
 
-Tokens.BRACKET_OPEN = "{";
-Tokens.BRACKET_CLOSE = "}";
-Tokens.PARENTHESIS_OPEN = "(";
-Tokens.PARENTHESIS_CLOSE = ")";
-Tokens.SQUARE_BRACKET_OPEN = "[";
-Tokens.SQUARE_BRACKET_CLOSE = "]";
+Tokens.LBRACKET = "{";
+Tokens.RBRACKET = "}";
+Tokens.LPAREN = "(";
+Tokens.RPAREN = ")";
+Tokens.LSQUARE = "[";
+Tokens.RSQUARE = "]";
+Tokens.COMMA = ",";
+Tokens.SEMICOLON = ";";
+Tokens.NEWLINE = "\n";
 
 
 const Operators = {};
 
+Operators.ASSIGN = "=";
+Operators.DIVIDE = "/";
 Operators.PLUS = "+";
 Operators.MINUS = "-";
+Operators.MULTIPLY = "*";
 Operators.AND = "&";
 Operators.OR = "|";
 Operators.NOT = "!";
 Operators.EQUALS = "=";
+Operators.LESS_THAN = "<";
+Operators.GREATER_THAN = ">";
 
 
 const Types = {};
 
-Types.String = "String";
-Types.Boolean = "Boolean";
-Types.Int = "Int";
-Types.Float = "Float";
-Types.Bit = "Bit";
-Types.Undefined = "undefined";
-Types.Null = "null";
-Types.Object = "Object";
+Types.AUTO = "auto";
+Types.STRING = "String";
+Types.BOOLEAN = "Boolean";
+Types.INT = "Int";
+Types.FLOAT = "Float";
+Types.BIT = "Bit";
+Types.UNDEFINED = "undefined";
+Types.NULL = "null";
+Types.OBJECT = "Object";
+Types.LIST = "List";
+Types.MAP = "Map";
+Types.DICT = Types.MAP;
+
 // Not sure if added
 Types.Symbol = "Symbol";
+
+console.log(Types['auto']);
+
+module.exports = {
+    Tokens,
+    Operators,
+    Types,
+};
